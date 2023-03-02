18b668c2d0da6d18a45ec92d00c7267747856108
jesandante007 <jesandante007@gmail.com>
2023-03-02 21:36:26 +0600

    appendChild to container

--- js/app.js
+++ js/app.js
@@ -12,7 +12,48 @@ const loadAIs = async () => {
 
 // display fetched data
 const showAIs = (AIs) => {
-  console.log(AIs);
+    
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = "";
+  AIs.forEach((AI) => {
+    const { id, name, description, image, published_in, features } = AI;
+    cardsContainer.innerHTML += `
+    <div class="col">
+        <div class="card h-100 p-3">
+        <img src="${image}" class="card-img-top rounded" alt="..." height="220"/>
+        <div class="card-body">
+            <h5 class="card-title">Features</h5>
+            <div class="card-text">
+                1. ${features[0]}<br>
+                2. ${features[1]}<br>
+                3. ${features[2]}
+            </div>
+        </div>
+        <div class="card-footer bg-body mt-3 d-flex justify-content-between align-items-center">
+            <div>
+                <h5 class="my-2">${name}</h5>
+                <p><i class="fa-solid fa-calendar-days"></i> ${published_in}</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-circle-arrow-right fs-2"></i>
+            </div>
+        </div>
+        </div>
+    </div>`;
+  });
+
+  //   stop spinner
+  toggleClass('spinner', false);
+};
+
+// toggleClass
+const toggleClass = (id, condition) => {
+  const element = document.getElementById(id);
+  if (condition) {
+    element.classList.remove("d-none");
+  } else {
+    element.classList.add("d-none");
+  }
 };
 
 loadAIs();
